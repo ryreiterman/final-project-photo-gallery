48b9d6e06c915f03a74c6a4dcd07dff279160c2a
Ryan Reiterman <ryreiterman@gmail.com>
2021-03-24 22:30:10 -0400

    Added my pics as placeholders

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,34 @@ import ImageGallery from 'react-image-gallery';
 export default function Home(props) {
 	const images = [
 		{
-			original: 'https://picsum.photos/id/1018/1000/600/',
-			thumbnail: 'https://picsum.photos/id/1018/250/150/'
+			original:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616552557/zoo-pics/brevard-zoo/vulture_web_xegfeh.jpg',
+			thumbnail:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616552557/zoo-pics/brevard-zoo/vulture_web_xegfeh.jpg'
 		},
 		{
-			original: 'https://picsum.photos/id/1015/1000/600/',
-			thumbnail: 'https://picsum.photos/id/1015/250/150/'
+			original:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616551204/zoo-pics/central-florida-zoo/boa_web_x1udvq.jpg',
+			thumbnail:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616551204/zoo-pics/central-florida-zoo/boa_web_x1udvq.jpg'
 		},
 		{
-			original: 'https://picsum.photos/id/1019/1000/600/',
-			thumbnail: 'https://picsum.photos/id/1019/250/150/'
+			original:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616551911/zoo-pics/brevard-zoo/bald_eagle_web_qcnnpn.jpg',
+			thumbnail:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616551911/zoo-pics/brevard-zoo/bald_eagle_web_qcnnpn.jpg'
+		},
+		{
+			original:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616550233/zoo-pics/animal-kingdom/meerkat_small_kwoya2.jpg',
+			thumbnail:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616550233/zoo-pics/animal-kingdom/meerkat_small_kwoya2.jpg'
+		},
+		{
+			original:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616552433/zoo-pics/brevard-zoo/jag_lick_web_fyn4jn.jpg',
+			thumbnail:
+				'https://res.cloudinary.com/ryanphotos/image/upload/v1616552433/zoo-pics/brevard-zoo/jag_lick_web_fyn4jn.jpg'
 		}
 	];
 
