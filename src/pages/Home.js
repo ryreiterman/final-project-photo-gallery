76b625e513af34dba2e9cf6be4991a7f04a4b0bc
Ryan Reiterman <ryreiterman@gmail.com>
2021-03-24 21:42:49 -0400

    Added react-image-gallery npm package

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,25 @@
 import React, { useState } from 'react';
+import ImageGallery from 'react-image-gallery';
 
 export default function Home(props) {
-	return <div className="HomePage">This is the {props.page} page</div>;
+	const images = [
+		{
+			original: 'https://picsum.photos/id/1018/1000/600/',
+			thumbnail: 'https://picsum.photos/id/1018/250/150/'
+		},
+		{
+			original: 'https://picsum.photos/id/1015/1000/600/',
+			thumbnail: 'https://picsum.photos/id/1015/250/150/'
+		},
+		{
+			original: 'https://picsum.photos/id/1019/1000/600/',
+			thumbnail: 'https://picsum.photos/id/1019/250/150/'
+		}
+	];
+
+	return (
+		<div>
+			<ImageGallery items={images} />
+		</div>
+	);
 }
