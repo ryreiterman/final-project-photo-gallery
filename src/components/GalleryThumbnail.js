c4d76a04f054d17c7d3eb53feb33e5bbc8c82796
Ryan Reiterman <ryreiterman@gmail.com>
2021-04-04 21:35:58 -0400

    Gallery thumbnail url dynamically linking based on tag name

--- src/components/GalleryThumbnail.js
+++ src/components/GalleryThumbnail.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 // import { Image } from 'cloudinary-react';
 // import Zoo from '../pages/Zoo';
-import config from '../config';
-import config2 from '../config2';
+// import config from '../config';
+// import config2 from '../config2';
 
 const GalleryThumbnail = props => {
-	const [gallery, setGallery] = useState(props.match.params.zoo);
-	const apiKey = window.btoa(config.apiKey);
+	const [gallery, setGallery] = useState([]);
 
 	// const galleries = ['/animalkingdom', '/brevardzoo', '/columbuszoo'];
 	// console.log(galleries);
@@ -18,8 +18,8 @@ const GalleryThumbnail = props => {
 			try {
 				const response = await fetch('/api/tags');
 				const data = await response.json();
-				console.log(data);
-				setGallery(data);
+				console.log(data.tags[1]);
+				setGallery(data.tags);
 			} catch (error) {
 				console.error(error);
 			}
@@ -36,12 +36,14 @@ const GalleryThumbnail = props => {
 		// </nav>
 		<>
 			<div className="thumbnail-container">
-				<a href="/animalkingdom">
+				<Link to={`/${gallery[0]}`}>
+					{/* <a href="/animalkingdom"> */}
 					<img
 						src="https://res.cloudinary.com/ryanphotos/image/upload/v1616550233/zoo-pics/animal-kingdom/tiger_headon_small_wp7jnk.jpg"
 						className="thumbnail"
 					/>
-				</a>
+					{/* </a> */}
+				</Link>
 			</div>
 		</>
 	);
