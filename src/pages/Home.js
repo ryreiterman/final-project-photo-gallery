842ba294ca29d04af7254e4124e023cb11cfa03b
Ryan Reiterman <ryreiterman@gmail.com>
2021-04-09 22:16:11 -0400

    Added comments to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import GalleryThumbnail from '../components/GalleryThumbnail';
 
+// Display all galleries from Cloudinary in grid of thumbnail images
+// Grab thumbnail image from 1st item in array, link to tag name, click on thumbnail takes you to gallery url
+
 export default function App(props) {
 	return (
 		<>
@@ -10,10 +13,3 @@ export default function App(props) {
 		</>
 	);
 }
-
-// Display all galleries from Cloudinary in grid of thumbnail images
-// Grab thumbnail image from 1st item in array, link to tag name, click on thumbnail takes you to gallery url
-
-//Gallery thumbnail React component
-
-//
